edfe788b037944b9d0c012bfc1001c26700857a9
KevinWong07 <70236929+KevinWong07@users.noreply.github.com>
2021-04-12 21:47:21 -0400

    Add files via upload

---
+++ Lab 8/index.js
@@ -0,0 +1,8 @@
+// grabs input from 'phone' element and runs it through function (e)
+document.getElementById('phone').addEventListener('input', function (e) {
+    // declares a variable 'x' and checks if input is a number
+    // if input is not number, it won't show up
+    var x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+    // checks inputs and if matches the above, it appends a '-' after each number group 
+    e.target.value = !x[2] ? x[1] : x[1] + '-' + x[2] + (x[3] ? '-' + x[3] : '');
+});
